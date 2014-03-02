805e15def5416fdd2c9dadc525048cddf15434e3
Alex Unger <aunger@aunger-macbookair.roam.corp.google.com>
2014-03-02 13:13:05 -0800

    move json-ld creation to a function

--- gigpress/output/gigpress_shows.php
+++ gigpress/output/gigpress_shows.php
@@ -198,29 +198,8 @@ function gigpress_shows($filter = null, $content = null) {
 					
 					include gigpress_template('shows-list');
 					
-					$show_markup = array("@context" => "http://schema.org", "@type" => "Event");
-					$show_markup['name'] = $showdata['artist_plain'];
-					$show_markup['startDate'] = $showdata['iso_date'];
-					//if(!empty($showdata['venue_url'])) { $show_markup['typicalAgeRange'] = $showdata['venue_url']; }
-					
-					
-					$location_markup = array("@type" => "Place");
-					$location_markup['name'] = $showdata['venue_plain'];
-					if(!empty($showdata['venue_url'])) { $location_markup['url'] = $showdata['venue_url']; }
-					if(!empty($showdata['venue_phone'])) { $location_markup['telephone'] = $showdata['venue_phone']; }
-					
-					$address_markup = array("@type" => "PostalAddress");
-					if(!empty($showdata['address_plain'])) { $address_markup['streetAddress'] = $showdata['address_plain']; }
-					$address_markup['addressLocality'] = $showdata['city'];
-					if(!empty($showdata['state'])) { $address_markup['addressRegion'] = $showdata['state']; }
-					if(!empty($showdata['postal_code'])) { $address_markup['postalCode'] = $showdata['postal_code']; }
-					if(!empty($showdata['country'])) { $address_markup['addressCountry'] = array("@type" => "Country", "name" => $showdata['country']); }
-					
-					$location_markup['address'] = $address_markup;
-					
-					$show_markup['location'] = $location_markup;
+					$show_markup = gigpress_json-ld($showdata);
 					array_push($shows_markup,$show_markup);
-
 				}
 				
 				include gigpress_template('shows-list-end');						
@@ -270,9 +249,14 @@ function gigpress_shows($filter = null, $content = null) {
 				$class .= ($showdata['tour'] && !$tour) ? ' gigpress-tour' : '';
 				
 				include gigpress_template('shows-list');
+				
+				$show_markup = gigpress_json-ld($showdata);
+				array_push($shows_markup,$show_markup);
 			}
 			
 			include gigpress_template('shows-list-end');
+			
+			$shows_markup_json = json_encode($shows_markup, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
 			include gigpress_template('shows-list-footer');			
 			
 		} else {
@@ -396,4 +380,30 @@ function gigpress_has_upcoming($filter = null)
 	if($shows) return true;
 	
 	
+}
+
+function gigpress_json-ld($showdata) {
+	$show_markup = array("@context" => "http://schema.org", "@type" => "Event");
+	$show_markup['name'] = $showdata['artist_plain'];
+	$show_markup['startDate'] = $showdata['iso_date'];
+	//if(!empty($showdata['venue_url'])) { $show_markup['typicalAgeRange'] = $showdata['venue_url']; }
+
+
+	$location_markup = array("@type" => "Place");
+	$location_markup['name'] = $showdata['venue_plain'];
+	if(!empty($showdata['venue_url'])) { $location_markup['url'] = $showdata['venue_url']; }
+	if(!empty($showdata['venue_phone'])) { $location_markup['telephone'] = $showdata['venue_phone']; }
+
+	$address_markup = array("@type" => "PostalAddress");
+	if(!empty($showdata['address_plain'])) { $address_markup['streetAddress'] = $showdata['address_plain']; }
+	$address_markup['addressLocality'] = $showdata['city'];
+	if(!empty($showdata['state'])) { $address_markup['addressRegion'] = $showdata['state']; }
+	if(!empty($showdata['postal_code'])) { $address_markup['postalCode'] = $showdata['postal_code']; }
+	if(!empty($showdata['country'])) { $address_markup['addressCountry'] = array("@type" => "Country", "name" => $showdata['country']); }
+
+	$location_markup['address'] = $address_markup;
+
+	$show_markup['location'] = $location_markup;
+	
+	return $show_markup;
 }
