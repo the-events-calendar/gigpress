18f2a5f056e760adcb9dff9578a77fac77870777
Res Rickli <rr@maerchenstund.ch>
2016-12-13 09:25:40 +0100

    Add files via upload

--- templates/shows-list.php
+++ templates/shows-list.php
@@ -4,6 +4,7 @@
 	If you wish to customize the output, you can safely do so by COPYING this file into a new folder called 'gigpress-templates' in your 'wp-content' directory	and then making your changes there. When in place, that file will load in place of this one.
 	
 	This template displays all of our individual show data in the main shows listing (upcoming and past).
+	At the end of this template you can also modify the contents if the InformationWindow appearing on the overview map for each venue.
 
 	If you're curious what all variables are available in the $showdata array, have a look at the docs: http://gigpress.com/docs/
 */
@@ -11,7 +12,7 @@
 
 <tbody>
 	
-	<tr class="gigpress-row <?php echo $class; ?>">
+	<tr id="<?php echo $showdata['id']; ?>" class="gigpress-row <?php echo $class; ?>">
 	
 		<td class="gigpress-date"><?php echo $showdata['date']; ?>
 			<?php if($showdata['end_date']) : ?> - <?php echo $showdata['end_date']; ?><?php endif; ?>
@@ -97,3 +98,26 @@
 	
 	</tr>
 </tbody>	
+<?php
+  if ($gigmap_thisGig!=null) { 
+		//In this section you can set the contents of the markers tooltip and InfoWindow of each location in the overview map.
+		//The marker's title will appear as a tooltip. Default: "City(State), Venue". May not contain HTML
+		$citystate=$showdata['city_plain'];
+		if (!empty($showdata['state'])) $citystate = $citystate.'('.$showdata['state'].')';  
+		$gigmap_thisGig->setGigmapMarkerTooltip($citystate.', '.$showdata['venue_plain']);
+		//
+		//The InfoWindow appears when the user clicks on the marker. Here we define its contents
+		//
+		//The next statement is used to set the first <div ...> statemant in the InfoWindow of each location in the overview map. 
+		//Example: to set the background-color to black use: 
+		//$gigmap_thisGig->setGigmapInfoWdwDiv('<div style="background-color: #000";>');
+		$gigmap_thisGig->setGigmapInfoWdwDiv('');
+		//First and second line of the InfoWindow
+		$gigmap_thisGig->setFirstLine('<h5 class="gigpress-heading">'.$showdata['city'].'</h5>');
+		$gigmap_thisGig->setSecondLine('<span class="gigpress-info-item"><strong>' . $showdata['venue'] . '</strong> ____ ' . $showdata['address_plain'] . ', ' . $showdata['city_plain'] . '&ensp;</span>');
+		//The next line sets the class for the appearance of the performance date(s)
+		$gigmap_thisGig->setDateClass('"gigpress-date"');  
+		//And this is the description of the performance itself:
+		$gigmap_thisGig->setPerformanceText('<span class="gigpress-info-item">' . '&emsp;' . $showdata['artist_plain'] . '&emsp;|&emsp;' . $showdata['tour'] . '&ensp;</span>');
+	}
+?>
