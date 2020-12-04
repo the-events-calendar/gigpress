87fedb22e9b55ca2f4091f74564f0717a8a2eed9
András Guseo <andras@divi-magazine.com>
2020-12-04 10:13:34 +0100

    Added parameter check to avoid PHP warning

--- lib/parsecsv.lib.php
+++ lib/parsecsv.lib.php
@@ -176,7 +176,12 @@ class parseCSV {
 	function __construct($input = null, $offset = null, $limit = null, $conditions = null) {
 		if ( $offset !== null ) $this->offset = $offset;
 		if ( $limit !== null ) $this->limit = $limit;
-		if ( count($conditions) > 0 ) $this->conditions = $conditions;
+		if (
+			$conditions !== null
+			&& count( $conditions ) > 0
+		) {
+			$this->conditions = $conditions;
+		}
 		if ( !empty($input) ) $this->parse($input);
 	}
 	
@@ -195,7 +200,12 @@ class parseCSV {
 		if ( !empty($input) ) {
 			if ( $offset !== null ) $this->offset = $offset;
 			if ( $limit !== null ) $this->limit = $limit;
-			if ( count($conditions) > 0 ) $this->conditions = $conditions;
+			if (
+				$conditions !== null
+				&& count( $conditions ) > 0
+			) {
+				$this->conditions = $conditions;
+			}
 			if ( is_readable($input) ) {
 				$this->data = $this->parse_file($input);
 			} else {
